91811abfdd5bf2e2775e4b9b01cce883fe7ee705
木田悠一郎 <yuichiro@y-kida-MacBook-Pro.local>
2017-11-02 12:32:34 +0900

    can mysqldump by specifying output directory

--- lib/rails_mysqldump.rb
+++ lib/rails_mysqldump.rb
@@ -1,5 +1,15 @@
 require "rails_mysqldump/version"
 
 module RailsMysqldump
-  # Your code goes here...
+  def self.run(path = 'tmp/mysqldump')
+    dump_dir = Rails.root.join(path)
+    FileUtils.mkdir_p(dump_dir)
+    Dir.chdir(dump_dir)
+
+    db_conf = Rails.application.config.database_configuration[Rails.env]
+    password_option = db_conf['password'].nil? ? "" : "-p'#{db_conf['password']}'"
+    output_file = "#{Rails.env}_#{Time.current.to_s(:number)}.dump"
+
+    system "mysqldump -u #{db_conf['username']} #{password_option} -h localhost #{db_conf['database']} --single-transaction > #{output_file}"
+  end
 end
